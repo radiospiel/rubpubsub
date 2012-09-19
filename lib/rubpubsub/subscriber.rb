5849bebd0b987985cb638161e4931b0f7fb65509
radiospiel <eno@open-lab.org>
2012-09-19 11:58:38 +0200

    Moved subscribe code into helper method, and split subscribe block into two methods.

--- lib/rubpubsub/subscriber.rb
+++ lib/rubpubsub/subscriber.rb
@@ -17,44 +17,48 @@ class RubPubSub::Subscriber < Sinatra::Base
     super
   end
   
-  get '/', provides: 'text/event-stream' do
-    stream :keep_open do |out|
-      keepalive_timer = nil
-      
-      reschedule_keepalive = lambda do
-        if keepalive_timer
-          keepalive_timer.cancel
-        end
-        
-        keepalive_timer = EventMachine::PeriodicTimer.new(28) do 
-          # If we would send a real SSE event, it could look like this:
-          #
-          # out << "event: keepalive\ndata: \n\n"
-          #
-          # However, according to the spec an empty line should be
-          # ignored by the client, and we just need to keep up the 
-          # connection, not to transfer any data to the client.
-          out << "\n" 
-        end
-      end
-      
-      reschedule_keepalive.call()
-      
-      subscription = @rubpubsub.subscribe params[:user], "chat" do |channel, data|
-        lines = data.split(/(\r\n|\r|\n)/)
-        lines[0] = "channel: #{channel} #{lines[0]}"
-        lines = lines.map { |line| "data: #{line}" }
-        out << lines.join("\n") << "\n\n"
-
-        # We just wrote some output, and therefore can reschedule the 
-        # keepalive timer.
-        reschedule_keepalive.call()
+  helpers do
+    def reschedule_keepalive(timer, out)
+      if timer
+        timer.cancel
       end
 
-      out.callback do
-        keepalive_timer.cancel
-        @rubpubsub.unsubscribe(subscription)
+      EventMachine::PeriodicTimer.new(28) do 
+        # If we would send a real SSE event, it could look like this:
+        #
+        # out << "event: keepalive\ndata: \n\n"
+        #
+        # However, according to the spec an empty line should be
+        # ignored by the client, and we just need to keep up the 
+        # connection, not to transfer any data to the client.
+        out << "\n" 
       end
     end
+    
+    def subscribe(*channels)
+      stream :keep_open do |out|
+        timer = reschedule_keepalive(timer, out)
+
+        subscription = @rubpubsub.subscribe params[:user], "chat" do |channel, data|
+          lines = data.split(/(\r\n|\r|\n)/)
+          lines[0] = "channel: #{channel} #{lines[0]}"
+          lines = lines.map { |line| "data: #{line}" }
+          out << lines.join("\n") << "\n\n"
+
+          # We just wrote some output, and therefore can reschedule the 
+          # keepalive timer.
+          timer = reschedule_keepalive(timer, out)
+        end
+
+        out.callback do
+          timer.cancel
+          @rubpubsub.unsubscribe(subscription)
+        end
+      end
+    end
+  end
+  
+  get '/', provides: 'text/event-stream' do
+    subscribe params[:user], "chat"
   end
 end
